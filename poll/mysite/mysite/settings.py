894795c423fb7d70253824c5325d06de2e186d80
jonathanchavez <jchavez2017@gmail.com>
2016-07-11 17:25:26 -0400

    Change database to postgresql, works

--- poll/mysite/mysite/settings.py
+++ poll/mysite/mysite/settings.py
@@ -77,11 +77,14 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hp',
+        'USER': 'jonathan',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
