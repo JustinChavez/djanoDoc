7fd340a5ce96752127751c64f4b9e9baa770f707
jonathanchavez <jchavez2017@gmail.com>
2016-07-19 13:27:12 -0400

    static

--- mysite/settings.py
+++ mysite/settings.py
@@ -125,4 +125,21 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
+# STATIC_URL = '/static/'
+
+
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+#was told to put this in here
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -10,7 +10,12 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
+
+#testing from django place
+# application = get_wsgi_application()
