1c460c8ccf88af730130d4ddd139428253413fc6
jonathanchavez <jchavez2017@gmail.com>
2016-07-19 12:30:12 -0400

    ss

--- mysite/local_settings.py
+++ mysite/local_settings.py
@@ -1,15 +1,15 @@
-import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'hp',
-        'USER': 'jonathan',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '',
-    }
-}
-
-DEBUG = True
+# import os
+# BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'hp',
+#         'USER': 'jonathan',
+#         'PASSWORD': '',
+#         'HOST': 'localhost',
+#         'PORT': '',
+#     }
+# }
+#
+# DEBUG = True
