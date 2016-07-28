060344c3d428c8b966c882113af16d0501da3540
jonathanchavez <jchavez2017@gmail.com>
2016-07-28 11:13:13 -0400

    Mergeing bad bad

--- poll/mysite/polls/models.py
+++ poll/mysite/polls/models.py
@@ -19,24 +19,16 @@ class Question(models.Model):
         #are not labeled as being published recently
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
-    was_published_recently.admin_order_field = 'pub_date'
-    was_published_recently.boolean = True
-    was_published_recently.short_description = 'Published recently?'
 
 @python_2_unicode_compatible
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
-    # user = models.ForeignKey(User)
     user = models.ForeignKey(to=User, related_name="use", blank=True, null=True)
     def __str__(self):
         return self.choice_text
 
-
 #User profile
 class UserProfile(models.Model):
     user = models.OneToOneField(User)
-
-
-# user = models.ForeignKey(User, on_delete=models.CASCADE)
